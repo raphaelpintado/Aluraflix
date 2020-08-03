9399f618391a81fcd7757295214f64ddd27b264d
Raphael Moraes <raphaelpintado@gmail.com>
2020-08-03 20:07:29 -0300

    roteamento e state

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Menu from './components/Menu'
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
-import dadosIniciais from './data/dados_iniciais.json';
+import Menu from '../../components/Menu'
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
+import dadosIniciais from '../../data/dados_iniciais.json';
 
-function App() {
+function Home() {
   return (
     <div style={{ background: "#141414" }}>
       <Menu />
@@ -28,8 +28,10 @@ function App() {
       <Carousel 
         category={dadosIniciais.categorias[2]}
       />
+
+      <Footer />
     </div>
   );
 }
 
-export default App;
+export default Home;
